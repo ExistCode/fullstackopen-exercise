e5b4c74ffd6c2684443a78c92fd07aee56915f61
Hans Andreanto <andreantohanss@gmail.com>
2024-01-31 22:00:23 +0700

    Exercise 1.5

--- part1/fullstackopen-exercises-project/src/App.jsx
+++ part1/fullstackopen-exercises-project/src/App.jsx
@@ -1,22 +1,24 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props) => {
-    return <h1>{props.course}</h1>;
+    return <h1>{props.course.name}</h1>;
   };
 
   const Content = (props) => {
@@ -31,7 +33,9 @@ const App = () => {
     return (
       <p>
         Number of Exercises{" "}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        {course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises}
       </p>
     );
   };
@@ -39,9 +43,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts[0]} />
-      <Content parts={parts[1]} />
-      <Content parts={parts[2]} />
+      <Content parts={course.parts[0]} />
+      <Content parts={course.parts[1]} />
+      <Content parts={course.parts[2]} />
       <Total />
     </div>
   );
