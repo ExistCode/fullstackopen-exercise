5c3ddc17ebe6361ec8c5e1621967c867af4d5581
Hans Andreanto <andreantohanss@gmail.com>
2024-02-05 23:53:43 +0700

    exercise 2.18 done

--- part2/countries/src/components/CountryForm.jsx
+++ part2/countries/src/components/CountryForm.jsx
@@ -4,13 +4,8 @@ const CountryForm = ({ country }) => {
       <h1>{country.name.common}</h1>
       <p>capital: {country.capital}</p>
       <p>area: {country.area}</p>
-      <h2>Spoken languages</h2>
-      <ul>
-        {country.languages.map((language) => (
-          <li key={language.name}>{language.name}</li>
-        ))}
-      </ul>
-      <img src={country.flag} alt="Country flag" width={100}></img>
+
+      <img src={country.flags.png} alt="Country flag" width={100}></img>
     </div>
   );
 };
